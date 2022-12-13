bc7d98309f83d393c1c68139286eda29d894db2e
Jaredzu <jared.zu96@gmail.com>
2022-12-13 17:42:36 -0500

    feat: added img and import on js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import api from "./api" // default import export
 import * as componets from "./components" // named import export
+import "./styles/styles.css"
+import "./styles/styles2.css"
+import messi from "./img/messicalvo.jpg"
 
 console.log("hello webpack")
 
 let data = api.httpGet()
 componets.createTitle(data)
 componets.createP(data)
+componets.createImg(messi)
